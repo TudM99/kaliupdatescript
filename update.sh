cda3f384fc91a070dacc5a5ef8a221fc82027e63
TudM99 <67870163+TudM99@users.noreply.github.com>
2023-01-13 20:22:19 +0200

    added reboot at the end

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Update script after first install
-apt-get update && apt-get full-upgrade -y && apt-get dist-upgrade -y && updatedb
+apt-get update && apt-get full-upgrade -y && apt-get dist-upgrade -y && updatedb && reboot -f
